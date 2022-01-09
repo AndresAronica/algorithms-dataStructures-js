2f510dc4036925c7e1e0d8ee135c1a50a99246d3
AndresAronica <andresaronica@gmail.com>
2022-01-08 22:17:08 -0300

    Added basic implementation of Binary Search Tree

--- DataStructures/5. binarySearchTree.js
+++ DataStructures/5. binarySearchTree.js
@@ -11,14 +11,104 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
+	// O(log n)
 	insert(value) {
 		let newNode = new Node(value);
 		if (this.root === null) {
 			this.root = newNode;
 			return this;
+		} else {
+			let current = this.root;
+
+			// Recorro el arbol buscando si puedo insertar el nodo, y donde
+			while (true) {
+				// Si es duplicado, no lo voy a insertar
+				if (value === current.value) return undefined;
+
+				// Si el nodo tiene que ir a la izquierda...
+				if (value < current.value) {
+					// Si no hay nada a la izquierda, lo inserto
+					if (current.left === null) {
+						current.left = newNode;
+						return this;
+					}
+					// Sino, el "iterador" pasa a la izquierda y sigue buscando
+					current = current.left;
+				} else { // Si el nodo tiene que ir a la derecha... misma logica
+					if (current.right === null) {
+						current.right = newNode;
+						return this;
+					}
+					current = current.right;
+				}
+			}
 		}
 
-		return this;
+	}
+
+	// O(log n)
+	find(value) {
+		// Implementacion del curso
+		if (this.root === null) return null;
+		if (this.root.value === value) return this.root;
+		let current = this.root,
+		found = false;
+
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value < current.value) {
+				current = current.right;
+			} else {
+				found = true;
+			}
+		}
+		if (!found) return undefined;
+		return current;
+
+		// Mi implementacion
+		/*
+		if (this.root === null) return null;
+		if (this.root.value === value) return this.root;
+		let current = this.root;
+		while(true) {
+			if (value <= current.value) {
+				if (current.value === value) return current;
+				if (current.left !== null) {
+					current = current.left;
+				}
+				else { 
+					return undefined;
+				}
+			} else {
+				if (value === current.value) return current;
+				if (current.right !== null) {
+					current = current.right;
+				} else {
+					return undefined;
+				}
+			}
+		}*/
+	}
+
+	// O(log n)
+	contains(value) {
+		// Implementacion del curso
+		if (this.root === null) return null;
+		if (this.root.value === value) return this.root;
+		let current = this.root,
+		found = false;
+
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value < current.value) {
+				current = current.right;
+			} else {
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
@@ -29,8 +119,10 @@ class BinarySearchTree {
 */
 
 let bstree = new BinarySearchTree();
-bstree.root = new Node(10);
-// bstree.root.right = new Node(15);
-// bstree.root.left = new Node(7);
-// bstree.root.left.right = new Node(9);
-// bstree.root.left.left = new Node(5);
+bstree.insert(10);
+bstree.insert(5);
+bstree.insert(13);
+bstree.insert(2);
+bstree.insert(7);
+bstree.insert(11);
+bstree.insert(16);
